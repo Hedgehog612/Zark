ae65b5c6e5b21980cde2b96f1824469265b7d3f8
Marcus Bamberger <marcusbamberger@Marcuss-MacBook-Pro.local>
2019-09-19 15:20:03 -0700

    Zark 0.01

---
+++ Zark/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Zark
+//
+//  Created by Marcus Bamberger on 9/19/19.
+//  Copyright © 2019 Marcus Bamberger. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
